c4755cc0941f235e76a262dcc48857b687d678bb
yuuki <24261057@kyoto-wu.ac.jp>
2026-07-16 15:19:43 +0900

    変更内容の説明

--- js/scenes/PlanetScene.js
+++ js/scenes/PlanetScene.js
@@ -4,6 +4,9 @@ import TimeManager from "../world/time.js";
 import TimeClock from "../ui/timeClock.js";
 import { GAME_SETTINGS } from "../data/settings.js";
 
+// ★ 先ほど作ったプレイヤー本体クラスをインポート
+import Player from "../player/player.js";
+
 export default class PlanetScene extends Phaser.Scene {
 
     constructor() {
@@ -11,13 +14,33 @@ export default class PlanetScene extends Phaser.Scene {
     }
 
     preload() {
+        // マップ画像のロード
         this.load.image(GAME_SETTINGS.map.dayKey, GAME_SETTINGS.map.dayPath);
         this.load.image(GAME_SETTINGS.map.nightKey, GAME_SETTINGS.map.nightPath);
+
+        // ★ キャラクター用アニメーション画像のロード
+        const basePath = 'assets/images/charactors/';
+        
+        // 下向き
+        this.load.image('player_down1', basePath + '下向き1.png');
+        this.load.image('player_down2', basePath + '下向き2.png');
+        this.load.image('player_down3', basePath + '下向き3.png');
+        this.load.image('player_down4', basePath + '下向き4.png');
+
+        // 左向き
+        this.load.image('player_left1', basePath + '左向き1.png');
+        this.load.image('player_left2', basePath + '左向き2.png');
+        this.load.image('player_left3', basePath + '左向き3.png');
+        this.load.image('player_left4', basePath + '左向き4.png');
+
+        // 上向き
+        this.load.image('player_up1', basePath + '上向き1.png');
+        this.load.image('player_up2', basePath + '上向き2.png');
+        this.load.image('player_up3', basePath + '上向き3.png');
+        this.load.image('player_up4', basePath + '上向き4.png');
     }
 
-
     create() {
-
         // ① マップを生成
         this.worldMap = new WorldMap(this);
 
@@ -38,48 +61,29 @@ export default class PlanetScene extends Phaser.Scene {
         // ④ スポーン地点を取得
         const spawn = this.worldMap.getSpawnPoint();
 
+        // ★ ⑤ 本物のプレイヤーを生成して配置
+        // スポーン地点（spawn.x, spawn.y）にプレイヤーを配置します
+        this.player = new Player(this, spawn.x, spawn.y);
+
+        // ★ ⑥ キーボード入力を取得する用のcursorsを作成
+        this.cursors = this.input.keyboard.createCursorKeys();
+
         console.log("マップ生成完了");
         console.log("スポーン地点:", spawn);
     }
-
 
     update(time, delta) {
         this.timeManager.update(delta);
         this.timeClock.update();
 
-        // ===== 仮キャラクター移動処理ここから（正式なmovement.js実装後に削除） =====
-        if (!this.player?.body) return;
+        // ★ ⑦ プレイヤーの更新処理（移動＆アニメーション）を呼び出す
+        if (this.player) {
+            // 本物の移動処理 (movement.js) を内部で呼び出します
+            this.player.update(this.cursors);
 
-        const left = this.cursors.left.isDown || this.wasdKeys.left.isDown;
-        const right = this.cursors.right.isDown || this.wasdKeys.right.isDown;
-        const up = this.cursors.up.isDown || this.wasdKeys.up.isDown;
-        const down = this.cursors.down.isDown || this.wasdKeys.down.isDown;
-
-        let dirX = 0;
-        let dirY = 0;
-
-        if (left) dirX -= 1;
-        if (right) dirX += 1;
-        if (up) dirY -= 1;
-        if (down) dirY += 1;
-
-        if (dirX !== 0 && dirY !== 0) {
-            const diagonal = Math.SQRT1_2;
-            dirX *= diagonal;
-            dirY *= diagonal;
+            // 【当たり判定連動】
+            // 移動後にもし障害物にぶつかっていたら、CollisionManager を使って進めないようにする
+            // ※CollisionManagerの仕様に合わせて、必要なら player.x や y を押し戻す処理を挟んでください
         }
-
-        const velocityX = dirX * TEMP_PLAYER_SPEED;
-        const velocityY = dirY * TEMP_PLAYER_SPEED;
-        const nextX = this.player.x + velocityX * (delta / 1000);
-        const nextY = this.player.y + velocityY * (delta / 1000);
-
-        if (this.collision.canMove(nextX, nextY)) {
-            this.player.body.setVelocity(velocityX, velocityY);
-        } else {
-            this.player.body.setVelocity(0, 0);
-        }
-        // ===== 仮キャラクター移動処理ここまで =====
     }
 }
-
